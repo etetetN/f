a0f54e00f25e4fa7fff952949527ac12c7909c15
etetetn <etaicalaora@gmail.com>
2023-12-03 11:29:45 +0200

    i add filter page stuff

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+function openNav() 
+{
+    document.getElementByClassNam("sidenav").style.width = "250px";
+}
+
+function closeNav() 
+{
+    document.getElementsByClassName("sidenav").style.width = "0";
+}
